eca80a44f856dda697a2147ad41e30179fff7647
Mathieu Poumeyrol <kali@zoy.org>
2015-06-17 17:32:55 +0200

    generate labels to cdb

---
+++ src/bin/wikidata_cdb.rs
@@ -0,0 +1,40 @@
+extern crate wolframite;
+extern crate tinycdb;
+
+use std::fs;
+use std::path;
+
+use tinycdb::Cdb;
+
+use wolframite::WikiError;
+use wolframite::helpers;
+use wolframite::wikidata;
+use wolframite::wikidata::EntityHelpers;
+
+pub use wolframite::wiki_capnp::monolingual_text as MongolingualText;
+
+pub type WikiResult<T> = Result<T,WikiError>;
+
+fn main() {
+    run().unwrap();
+}
+
+fn run() -> WikiResult<()> {
+    let date:String = helpers::latest("cap", "wikidata").unwrap().unwrap();
+    let target_root = helpers::data_dir_for("labels", "wikidata", &*date);
+    try!(fs::create_dir_all(target_root.clone()));
+    let filename = target_root+"/labels";
+    Cdb::new(path::Path::new(&*filename), |creator| {
+        let it = wikidata::for_date(&*date);
+        for (i, message) in it.unwrap().enumerate() {
+            let message = message.unwrap();
+            creator.add(&*message.get_id().unwrap().as_bytes(),
+                &*message.get_label("en").unwrap().unwrap_or("<no en label>".to_string()).as_bytes()
+            ).unwrap();
+            if i % 100000 == 0 {
+                println!("done {}", i);
+            }
+        }
+    }).unwrap();
+    Ok( () )
+}
