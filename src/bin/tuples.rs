4e113ae7292382c12659ed926f667fc3dd4a16f5
Mathieu Poumeyrol <kali@zoy.org>
2015-06-22 11:55:55 +0200

    with functions

--- src/bin/tuples.rs
+++ src/bin/tuples.rs
@@ -10,24 +10,30 @@ fn main() {
     run().unwrap();
 }
 
+fn reducer<T:Iterator<Item=u64>>(it:T) -> u64 {
+    it.fold(0,|a,b| a+b)
+}
+
+fn mapper<'a,T:Iterator<Item=wikidata::WikidataTriplet>>(it:T) -> Box<Iterator<Item=u64>+'a> 
+    where T:'a
+{
+    Box::new(it.filter(|t|
+       t.1 == wikidata::EntityRef::P(106) &&
+       t.2 == wikidata::EntityRef::Q(3399)
+    ).map(|_| 1u64))
+}
 
 fn run() -> WikiResult<()> {
-    let block_counter = |it:wikidata::BoxedIter<wikidata::WikidataTriplet>| -> u64 {
-        let sub = it.map(|t:wikidata::WikidataTriplet| {
-            if  t.1 == wikidata::EntityRef::P(106) &&
-                t.2 == wikidata::EntityRef::Q(33999) {
-                1u64
-            } else {
-                0u64
-            }
-        });
-        sub.fold(0,|a,b|a+b)
-    };
     let wd = wikidata::Wikidata::latest_compiled().unwrap();
     let mut pool = Pool::new(1 + num_cpus::get());
     let chunks = try!(wd.triplets_iter_iter());
+
+    let block_counter = |it:wikidata::BoxedIter<wikidata::WikidataTriplet>| -> u64 {
+        reducer(mapper(it))
+    };
     let halfway = unsafe { pool.map(chunks, &block_counter) };
-    let count:u64 = halfway.fold(0,|a,b|a+b);
+    let count = reducer(halfway);
+
     println!("count: {}", count);
     Ok( () )
 }
