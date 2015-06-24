707a5ee42cf96801174bf60274f3b8ed9cc82be4
Mathieu Poumeyrol <kali@zoy.org>
2015-06-24 14:18:23 +0200

    wip on generic m/r

--- src/bin/stats_cine.rs
+++ src/bin/stats_cine.rs
@@ -11,12 +11,39 @@ extern crate rustc_serialize;
 use simple_parallel::pool::Pool;
 use wolframite::WikiResult;
 use wolframite::wikidata;
+use wolframite::BoxedIter;
 use wolframite::wikidata::EntityHelpers;
 
 use wolframite::wikidata::EntityRef;
 
-
 fn main() { count().unwrap() }
+
+type BI<A> = Box<Iterator<Item=A>+Send>;
+
+struct MapReduceOp<M, R>
+    where   M: 'static + Sync + Fn(usize) -> usize,
+            R: 'static + Sync + Fn(BI<usize>) -> usize
+{
+    mapper:&'static M,
+    reducer:&'static R
+}
+
+impl <M,R> MapReduceOp<M,R>
+    where   M: 'static + Sync + Fn(usize) -> usize,
+            R: 'static + Sync + Fn(BI<usize>) -> usize
+{
+    fn run(&self, chunks:BI<BI<usize>>) -> usize {
+        let mut pool = Pool::new(1 + num_cpus::get());
+        let mapper = self.mapper;
+        let reducer = self.reducer;
+        let each = move |it:BI<usize>| -> usize {
+            reducer(Box::new(it.map(move |e| { mapper(e) })))
+        };
+        let counters:Vec<usize> = unsafe { pool.map(chunks, &each).collect() };
+        (self.reducer)(Box::new(counters.into_iter()))
+    }
+}
+
 
 #[allow(dead_code)]
 fn count() -> WikiResult<()> {
@@ -24,21 +51,28 @@ fn count() -> WikiResult<()> {
     let mut pool = Pool::new(1 + num_cpus::get());
     let chunks = try!(wd.entity_iter_iter());
 
-    let each = |it:Box<Iterator<Item=WikiResult<wikidata::MessageAndEntity>>+Send>| -> usize {
-        it.map(|e| {
-            let e = e.unwrap();
-            if e.get_relations().unwrap().any(|t|
-                t.0 == EntityRef::P(31) && t.1 == EntityRef::Q(11424) &&
-                e.get_claim(EntityRef::P(1258)).unwrap().is_some()
-            ) {
-                1
-            } else {
-                0
-            }
-        }).fold(0, |a,b|a+b)
+    fn mapper(e:WikiResult<wikidata::MessageAndEntity>) -> usize {
+        let e = e.unwrap();
+        if e.get_relations().unwrap().any(|t|
+            t.0 == EntityRef::P(31) && t.1 == EntityRef::Q(11424) &&
+            e.get_claim(EntityRef::P(1258)).unwrap().is_some()
+        ) {
+            1
+        } else {
+            0
+        }
+    }
+
+    let reducer = |it:BoxedIter<usize>| -> usize {
+        it.fold(0, |a,b| a+b)
     };
+
+    let each = |it:Box<Iterator<Item=WikiResult<wikidata::MessageAndEntity>>+Send>| -> usize {
+        reducer(Box::new(it.map(|e| { mapper(e) })))
+    };
+
     let counters:Vec<usize> = unsafe { pool.map(chunks, &each).collect() };
-    println!("{}", counters.iter().fold(0usize, |a,b| a+b));
+    println!("{}", reducer(Box::new(counters.into_iter())));
     Ok( () )
 }
 
