680cb95e0dc7193a4be46f5d8a6231d61e0edd57
Mathieu Poumeyrol <kali@zoy.org>
2015-07-14 20:29:46 +0200

    american presidents example

---
+++ src/bin/american_presidents.rs
@@ -0,0 +1,37 @@
+#![allow(non_snake_case)]
+
+extern crate wolframite;
+
+use wolframite::WikiResult;
+use wolframite::wikidata;
+use wolframite::wikidata::EntityHelpers;
+use wolframite::mapred;
+use wolframite::wikidata::EntityRef;
+
+fn main() { count().unwrap() }
+
+fn count() -> WikiResult<()> {
+    let wd = wikidata::Wikidata::latest_compiled().unwrap();
+
+    mapred::par_foreach(wd.entity_iter_iter().unwrap(),
+        |e:WikiResult<wikidata::MessageAndEntity>| {
+            let e = e.unwrap();
+            let mut relations = e.get_relations().unwrap();
+            if relations.any(|t| (t.0 == EntityRef::P(39) && t.1 == EntityRef::Q(11696))) {
+                let mut relations = e.get_relations().unwrap();
+                let fiction = relations.any(|t| t == (EntityRef::P(31), EntityRef::Q(95074)));
+                if fiction {
+                    let mut wd2 = wikidata::Wikidata::latest_compiled().unwrap();
+                    let work = e.get_relations().unwrap()
+                       .find(|t| t.0 == EntityRef::P(1441))
+                       .and_then(|t| wd2.get_label(&*t.1.get_id()));
+                    println!("{}, {}", e.get_a_label().unwrap(), work.unwrap_or("unknown work"));
+                } else {
+                    println!("{}, real", e.get_a_label().unwrap());
+                }
+            }
+        }
+    );
+    Ok(())
+}
+
